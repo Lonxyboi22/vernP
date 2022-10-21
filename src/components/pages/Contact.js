685c085b726d96ce02b0455d2e6873ef4735b49e
Lonxyboi <drewthemachine22@gmail.com>
2022-10-20 22:42:18 -0400

    finished the contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,70 +1,118 @@
-import { Container, Row, Col } from "react-bootstrap";
-import { useState } from "react";
-import { send } from "emailjs-com";
+import React from 'react';
+import * as emailjs from 'emailjs-com';
 
-const Contact = (props) => {
+import { Field, Label, Control, Input, Button, Icon, Textarea, Notification } from 'rbx';
 
-    const [toSend, setToSend] = useState({
-        from_name: '',
-        to_name: '',
-        message: '',
-        reply_to: '',
-      });
-    
-      const onSubmit = (e) => {
-        e.preventDefault();
-    send(
-      'SERVICE ID',
-      'TEMPLATE ID',
-      toSend,
-      'User ID'
-    )
-      .then((response) => {
-        console.log('SUCCESS!', response.status, response.text);
-      })
-      .catch((err) => {
-        console.log('FAILED...', err);
-      });
-      };
-    
-      const handleChange = (e) => {
-        setToSend({ ...toSend, [e.target.name]: e.target.value });
-      };
-    return (
-      <div className="contact-form">
-        <form onSubmit={onSubmit}>
-  <input
-    type='text'
-    name='from_name'
-    placeholder='from name'
-    value={toSend.from_name}
-    onChange={handleChange}
-  />
-  <input
-    type='text'
-    name='to_name'
-    placeholder='to name'
-    value={toSend.to_name}
-    onChange={handleChange}
-  />
-  <input
-    type='text'
-    name='message'
-    placeholder='Your message'
-    value={toSend.message}
-    onChange={handleChange}
-  />
-  <input
-    type='text'
-    name='reply_to'
-    placeholder='Your email'
-    value={toSend.reply_to}
-    onChange={handleChange}
-  />
-</form>
-      </div>
-      
-      )
+class ContactForm extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      subject: '',
+      message: ''
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.resetForm = this.resetForm.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
-  
-  export default Contact
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const { name, email, subject, message } = this.state;
+    const templateParams = {
+      from_name: name,
+      from_email: email,
+      subject,
+      message_html: message,
+    };
+    emailjs.send(
+      'service_r3zxxgq',
+      'template_kkqqiqv',
+       templateParams,
+      'nYtN2nfA6y54TZ9Nn'
+    )
+    this.resetForm();
+  };
+
+  resetForm() {
+    this.setState({
+      name: '',
+      email: '',
+      subject: '',
+      message: '',
+    });
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  render() {
+    const { name, email, subject, message, sentMessage } = this.state;
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <Field>
+          <Label>Name</Label>
+          <Control>
+            <Input
+              name="name"
+              type="text"
+              placeholder="Your first and last name"
+              value={name}
+              onChange={this.handleChange}
+            />
+          </Control>
+        </Field>
+        <Field>
+          <Label>Email for contact</Label>
+          <Control>
+            <Input
+              name="email"
+              type="email"
+              placeholder="email@gmail.com"
+              value={email}
+              onChange={this.handleChange}
+            />
+          </Control>
+        </Field>
+        <Field>
+          <Label>Subject</Label>
+          <Control>
+            <Input
+              name="subject"
+              type="text"
+              placeholder="What is the subject?"
+              value={subject}
+              onChange={this.handleChange}
+            />
+          </Control>
+        </Field>
+        <Field>
+          <Label>Message</Label>
+          <Control>
+            <Textarea
+              name="message"
+              placeholder="Tell me more about..."
+              value={message}
+              onChange={this.handleChange}
+            />
+          </Control>
+        </Field>
+
+        <Field kind="group">
+          <Control>
+            <Button color="dark">Send</Button>
+          </Control>
+          <Control>
+            <Button text>Cancel</Button>
+          </Control>
+        </Field>
+      </form>
+    );
+  }
+}
+
+export default ContactForm;
