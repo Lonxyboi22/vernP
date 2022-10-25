1a8871ee99abe8cdaa50fdd940aec79bbffb5255
Lonxyboi <drewthemachine22@gmail.com>
2022-10-25 15:19:17 -0400

    added more formatting to the home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,21 +1,35 @@
 import "bulma/css/bulma.min.css";
 
-import { Container, Row, Col } from "react-bootstrap";
-
 const Home = (props) => {
     return (
     <div>
-        <div className="columns is-vcentered">
-            <div className="column is-3">
+        <div className="columns">
+            <div className=" column is-3">
                 <img style={{ width: 200, height: 200 }} src={require('../images/placeholder.png')}>
                 </img>
-                <p className="bd-notification">-Vern Planck</p>
+                <p className="bd-notification">Bakkstabbath<br/>-Vern Planck</p>
             </div>
             <div className="column is-4">
-                <p className="bd-notification">This column with more content. this is going to be used for a bio ish thing</p>
+                <p className="bd-notification">This column with more content. this is going to be used for a bio ish thing/short blurb or quote</p>
             </div>
             <div className="column">
                 <p>This is for social media links</p>
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                <ul className="footer-icons" style={{ listStyleType: "none", margin: 0, padding: 0, overflow: "hidden"}}>
+                <li className="social-icons" style={{   float: "left" }}>
+                    <a href="https://www.linkedin.com/in/vern-planck/" style={{ display:"block", padding: 10 }}>
+                        <img style={{ width: 50, height: 50 }} src={require('../images/linkedin.png')}>
+                        </img>
+                    </a>
+                </li>
+                <li className="social-icons" style={{   float: "left" }}>
+                    <a href="https://www.instagram.com/bakstabbath.art/" style={{ display:"block", padding: 10 }}>
+                        <img style={{ width: 50, height: 50 }} src={require('../images/instagram.png')}>
+                        </img>
+                    </a>
+                </li>
+            </ul>
+            </div>
             </div>
         </div>
     </div>
