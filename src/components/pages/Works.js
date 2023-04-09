203a4e740796c88caafbe4186bd1746bdda2c929
Lonxyboi <drewthemachine22@gmail.com>
2023-04-09 16:21:18 -0400

    adding a carousel instead of other type

--- src/components/pages/Works.js
+++ src/components/pages/Works.js
@@ -1,12 +1,52 @@
 import React, { Component } from 'react';
 import { Container, Card} from "react-bootstrap"
+import { Modal } from 'react-bootstrap';
 
 
 const Works = (props) => {
+
     return(
     <Container fluid className="project-section">
-        <Container className= "project-container">
+        <Container className= "project-container" id='gallery' data-toggle='modal' data-target='#imageModal'>
             <Card className="project-card1">
+                <div className='modal fade' id='imageModal' tabIndex='-1' role='dialog' aria-hidden='true'>
+                    <div className='modal-content'>
+                        <div className="modal-header">
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                             <span aria-hidden="true"></span>
+                            </button>
+                        </div>
+                        <div>
+                        <div id="imageCarousel" className="carousel slide" data-ride="carousel">
+                            <div className="carousel-inner">
+                                <div className="carousel-item active">
+                                    <img className="d-block w-100" src="../images/Ovary2021.png"/>
+                                </div>
+                                <div className="carousel-item">
+                                    <img className="d-block w-100" src="../images/2021EyeIllustration.png"/>
+                                </div>
+                                <div className="carousel-item">
+                                    <img className="d-block w-100" src="../images/imagesStill-life.png"/>
+                                </div>
+                                <div className="carousel-item">
+                                  <img className="d-block w-100" src="../images/imagesStill-life.png"/>
+                                </div>
+                            </div>
+                            <a className="carousel-control-prev" href="#carouselExample" role="button" data-slide="prev">
+                                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span className="sr-only">Previous</span>
+                            </a>
+                            <a className="carousel-control-next" href="#carouselExample" role="button" data-slide="next">
+                                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span className="sr-only">Next</span>
+                            </a>
+                        </div>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
                 <Card.Body className="card-body">
                         <img style={{ width: 200, height: 300 }} src={require('../images/Ovary2021.png')}></img>
                 </Card.Body>
